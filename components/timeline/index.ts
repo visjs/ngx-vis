ee481bdfc83ce2ad9d90d933e1698f54ecb7677e
Paul Hindenberg <Paul.Hindenberg@gmail.com>
2018-02-22 10:49:48 +0100

    renaming based on new version
    renaming VisTimelineFitOptions to VisTimelineAnimationOptions as up on Vis Version 4.21

--- components/timeline/index.ts
+++ components/timeline/index.ts
@@ -1,182 +1,182 @@
 import * as Vis from 'vis';
 
 export type VisId = Vis.IdType;
-export interface VisTimelineItem extends Vis.DataItem {}
-export interface VisTimelineGroup extends Vis.DataGroup {}
-export interface VisDataSetOptions extends Vis.DataSetOptions {}
-export interface VisTimelineOptions extends Vis.TimelineOptions {}
+export interface VisTimelineItem extends Vis.DataItem { }
+export interface VisTimelineGroup extends Vis.DataGroup { }
+export interface VisDataSetOptions extends Vis.DataSetOptions { }
+export interface VisTimelineOptions extends Vis.TimelineOptions { }
 export class VisTimelineItems extends Vis.DataSet<VisTimelineItem> {
-    public constructor(data?: VisTimelineItem[], options?: VisDataSetOptions) {
-        super(data, options);
-    }
+  public constructor(data?: VisTimelineItem[], options?: VisDataSetOptions) {
+    super(data, options);
+  }
 
-    public getLength(): number {
-        return this.length;
-    }
+  public getLength(): number {
+    return this.length;
+  }
 
-    public add(data: VisTimelineItem | VisTimelineItem[], senderId?: VisId): VisId[] {
-        return super.add(data, senderId);
-    }
+  public add(data: VisTimelineItem | VisTimelineItem[], senderId?: VisId): VisId[] {
+    return super.add(data, senderId);
+  }
 
-    public clear(senderId?: VisId): VisId[] {
-        return super.clear(senderId);
-    }
+  public clear(senderId?: VisId): VisId[] {
+    return super.clear(senderId);
+  }
 
-    public distinct(field: string): any[] {
-        return super.distinct(field);
-    }
+  public distinct(field: string): any[] {
+    return super.distinct(field);
+  }
 
-    public flush(): void {
-        super.flush();
-    }
+  public flush(): void {
+    super.flush();
+  }
 
-    public forEach(callback: (item: VisTimelineItem, id: VisId) => void, options?: VisItemSelectionOptions): void {
-        super.forEach(callback, options);
-    }
+  public forEach(callback: (item: VisTimelineItem, id: VisId) => void, options?: VisItemSelectionOptions): void {
+    super.forEach(callback, options);
+  }
 
-    public getAll(options?: VisItemSelectionOptions): VisTimelineItem[] {
-        return super.get(options);
-    }
+  public getAll(options?: VisItemSelectionOptions): VisTimelineItem[] {
+    return super.get(options);
+  }
 
-    public getById(id: VisId, options?: VisItemSelectionOptions): VisTimelineItem {
-        return super.get(id, options);
-    }
+  public getById(id: VisId, options?: VisItemSelectionOptions): VisTimelineItem {
+    return super.get(id, options);
+  }
 
-    public getByIds(ids: VisId[], options?: VisItemSelectionOptions): VisTimelineItem[] {
-        return super.get(ids, options);
-    }
+  public getByIds(ids: VisId[], options?: VisItemSelectionOptions): VisTimelineItem[] {
+    return super.get(ids, options);
+  }
 
-    public getDataSet(): VisTimelineItems {
-        return super.getDataSet() as VisTimelineItems;
-    }
+  public getDataSet(): VisTimelineItems {
+    return super.getDataSet() as VisTimelineItems;
+  }
 
-    public getIds(options?: VisItemSelectionOptions): VisId[] {
-        return super.getIds(options);
-    }
+  public getIds(options?: VisItemSelectionOptions): VisId[] {
+    return super.getIds(options);
+  }
 
-    public map(callback: (item: VisTimelineItem, id: VisId) => any, options?: VisItemSelectionOptions): any[] {
-        return super.map(callback, options);
-    }
+  public map(callback: (item: VisTimelineItem, id: VisId) => any, options?: VisItemSelectionOptions): any[] {
+    return super.map(callback, options);
+  }
 
-    public max(field: string): VisTimelineItem {
-        return super.max(field);
-    }
+  public max(field: string): VisTimelineItem {
+    return super.max(field);
+  }
 
-    public min(field: string): VisTimelineItem {
-        return super.min(field);
-    }
+  public min(field: string): VisTimelineItem {
+    return super.min(field);
+  }
 
-    public on(event: string, callback: (event: string, properties: any, senderId: VisId) => void): void {
-        super.on(event, callback);
-    }
+  public on(event: string, callback: (event: string, properties: any, senderId: VisId) => void): void {
+    super.on(event, callback);
+  }
 
-    public off(event: string, callback: (event: string, properties: any, senderId: VisId) => void): void {
-        super.off(event, callback);
-    }
+  public off(event: string, callback: (event: string, properties: any, senderId: VisId) => void): void {
+    super.off(event, callback);
+  }
 
-    public removeItems(ids: VisId[], senderId?: VisId): VisId[] {
-        return super.remove(ids, senderId);
-    }
+  public removeItems(ids: VisId[], senderId?: VisId): VisId[] {
+    return super.remove(ids, senderId);
+  }
 
-    public setOptions(options?: VisDataSetQueueOptions): void {
-        super.setOptions(options);
-    }
+  public setOptions(options?: VisDataSetQueueOptions): void {
+    super.setOptions(options);
+  }
 
-    public update(data: VisTimelineItem[], senderId?: VisId): VisId[] {
-        return super.update(data, senderId);
-    }
+  public update(data: VisTimelineItem[], senderId?: VisId): VisId[] {
+    return super.update(data, senderId);
+  }
 }
 export class VisTimelineGroups extends Vis.DataSet<VisTimelineGroup> {
-    public constructor(data?: VisTimelineGroup[], options?: VisDataSetOptions) {
-        super(data, options);
-    }
+  public constructor(data?: VisTimelineGroup[], options?: VisDataSetOptions) {
+    super(data, options);
+  }
 
-    public getLength(): number {
-        return this.length;
-    }
+  public getLength(): number {
+    return this.length;
+  }
 
-    public add(data: VisTimelineGroup | VisTimelineGroup[], senderId?: VisId): VisId[] {
-        return super.add(data, senderId);
-    }
+  public add(data: VisTimelineGroup | VisTimelineGroup[], senderId?: VisId): VisId[] {
+    return super.add(data, senderId);
+  }
 
-    public clear(senderId?: VisId): VisId[] {
-        return super.clear(senderId);
-    }
+  public clear(senderId?: VisId): VisId[] {
+    return super.clear(senderId);
+  }
 
-    public distinct(field: string): any[] {
-        return super.distinct(field);
-    }
+  public distinct(field: string): any[] {
+    return super.distinct(field);
+  }
 
-    public flush(): void {
-        super.flush();
-    }
+  public flush(): void {
+    super.flush();
+  }
 
-    public forEach(callback: (item: VisTimelineGroup, id: VisId) => void, options?: VisGroupSelectionOptions): void {
-        super.forEach(callback, options);
-    }
+  public forEach(callback: (item: VisTimelineGroup, id: VisId) => void, options?: VisGroupSelectionOptions): void {
+    super.forEach(callback, options);
+  }
 
-    public getAll(options?: VisGroupSelectionOptions): VisTimelineGroup[] {
-        return super.get(options);
-    }
+  public getAll(options?: VisGroupSelectionOptions): VisTimelineGroup[] {
+    return super.get(options);
+  }
 
-    public getById(id: VisId, options?: VisGroupSelectionOptions): VisTimelineGroup {
-        return super.get(id, options);
-    }
+  public getById(id: VisId, options?: VisGroupSelectionOptions): VisTimelineGroup {
+    return super.get(id, options);
+  }
 
-    public getByIds(ids: VisId[], options?: VisGroupSelectionOptions): VisTimelineGroup[] {
-        return super.get(ids, options);
-    }
+  public getByIds(ids: VisId[], options?: VisGroupSelectionOptions): VisTimelineGroup[] {
+    return super.get(ids, options);
+  }
 
-    public getDataSet(): VisTimelineGroups {
-        return super.getDataSet() as VisTimelineGroups;
-    }
+  public getDataSet(): VisTimelineGroups {
+    return super.getDataSet() as VisTimelineGroups;
+  }
 
-    public getIds(options?: VisGroupSelectionOptions): VisId[] {
-        return super.getIds(options);
-    }
+  public getIds(options?: VisGroupSelectionOptions): VisId[] {
+    return super.getIds(options);
+  }
 
-    public map(callback: (item: VisTimelineGroup, id: VisId) => any, options?: VisGroupSelectionOptions): any[] {
-        return super.map(callback, options);
-    }
+  public map(callback: (item: VisTimelineGroup, id: VisId) => any, options?: VisGroupSelectionOptions): any[] {
+    return super.map(callback, options);
+  }
 
-    public max(field: string): VisTimelineGroup {
-        return super.max(field);
-    }
+  public max(field: string): VisTimelineGroup {
+    return super.max(field);
+  }
 
-    public min(field: string): VisTimelineGroup {
-        return super.min(field);
-    }
+  public min(field: string): VisTimelineGroup {
+    return super.min(field);
+  }
 
-    public on(event: string, callback: (event: string, properties: any, senderId: VisId) => void): void {
-        super.on(event, callback);
-    }
+  public on(event: string, callback: (event: string, properties: any, senderId: VisId) => void): void {
+    super.on(event, callback);
+  }
 
-    public off(event: string, callback: (event: string, properties: any, senderId: VisId) => void): void {
-        super.off(event, callback);
-    }
+  public off(event: string, callback: (event: string, properties: any, senderId: VisId) => void): void {
+    super.off(event, callback);
+  }
 
-    public removeItems(ids: VisId[], senderId?: VisId): VisId[] {
-        return super.remove(ids, senderId);
-    }
+  public removeItems(ids: VisId[], senderId?: VisId): VisId[] {
+    return super.remove(ids, senderId);
+  }
 
-    public setOptions(options?: VisDataSetQueueOptions): void {
-        super.setOptions(options);
-    }
+  public setOptions(options?: VisDataSetQueueOptions): void {
+    super.setOptions(options);
+  }
 
-    public update(data: VisTimelineGroup[], senderId?: VisId): VisId[] {
-        return super.update(data, senderId);
-    }
+  public update(data: VisTimelineGroup[], senderId?: VisId): VisId[] {
+    return super.update(data, senderId);
+  }
 }
 
-export interface VisDataSetQueueOptions extends Vis.DataSetQueueOptions {}
-export interface VisItemSelectionOptions extends Vis.DataSelectionOptions<VisTimelineItem> {}
-export interface VisGroupSelectionOptions extends Vis.DataSelectionOptions<VisTimelineGroup> {}
+export interface VisDataSetQueueOptions extends Vis.DataSetQueueOptions { }
+export interface VisItemSelectionOptions extends Vis.DataSelectionOptions<VisTimelineItem> { }
+export interface VisGroupSelectionOptions extends Vis.DataSelectionOptions<VisTimelineGroup> { }
 export type VisDate = Vis.DateType;
 export type VisTimelineEvents = Vis.TimelineEvents;
-export interface VisTimelineFitOptions extends Vis.TimelineFitOptions {}
-export interface VisTimelineEventPropertiesResult extends  Vis.TimelineEventPropertiesResult {}
+export interface VisTimelineAnimationOptions extends Vis.TimelineAnimationOptions { }
+export interface VisTimelineEventPropertiesResult extends Vis.TimelineEventPropertiesResult { }
 
-export class VisTimeline extends Vis.Timeline {}
+export class VisTimeline extends Vis.Timeline { }
 
 export * from './vis-timeline.service';
 export * from './vis-timeline.directive';
