7162bbd31b8fa8bc478cdbf9d0a7292f420382fa
Reinhardt, Martin <contact@martinreinhardt-online.de>
2018-10-20 17:27:34 +0200

    feat(Angular): Drop support for older version of Angular
    BREAKING CHANGE: The package will only support Angular 5+ because with this version metadata v4 was introduced, see #115

--- ngx-vis.js
+++ ngx-vis.js
@@ -1,4 +1,10 @@
 "use strict";
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
 function __export(m) {
     for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
 }
@@ -10,13 +16,13 @@ __export(require("./components/index"));
 var VisModule = /** @class */ (function () {
     function VisModule() {
     }
-    VisModule.decorators = [
-        { type: core_1.NgModule, args: [{
-                    declarations: [index_1.VisNetworkDirective, index_2.VisTimelineDirective],
-                    exports: [index_1.VisNetworkDirective, index_2.VisTimelineDirective],
-                    providers: [index_1.VisNetworkService, index_2.VisTimelineService],
-                },] },
-    ];
+    VisModule = __decorate([
+        core_1.NgModule({
+            declarations: [index_1.VisNetworkDirective, index_2.VisTimelineDirective],
+            exports: [index_1.VisNetworkDirective, index_2.VisTimelineDirective],
+            providers: [index_1.VisNetworkService, index_2.VisTimelineService],
+        })
+    ], VisModule);
     return VisModule;
 }());
 exports.VisModule = VisModule;
