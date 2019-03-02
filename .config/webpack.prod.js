ee6981b7995ec1b4d7f3db1ab8087e8bfad6b1ed
Martin Reinhardt <contact@martinreinhardt-online.de>
2019-03-02 08:04:50 +0100

    fix(Demo): Use relative paths

--- .config/webpack.prod.js
+++ .config/webpack.prod.js
@@ -11,7 +11,7 @@ module.exports = webpackMerge(commonConfig, {
 
   output: {
     path: helpers.root('docs'),
-    publicPath: './',
+    publicPath: '',
     filename: '[name].[hash].js',
     chunkFilename: '[id].[hash].chunk.js'
   },
