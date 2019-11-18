3b993451a54cab073e15d5a82cc64833135b7ef4
Martin Reinhardt <contact@martinreinhardt-online.de>
2019-11-18 06:56:43 +0100

    fix(Build): Resolve packaging via ng-packagr (#348)

--- components/network/vis-network.directive.ts
+++ components/network/vis-network.directive.ts
@@ -1,6 +1,6 @@
 import { Directive, ElementRef, EventEmitter, Input, OnChanges, OnDestroy, OnInit, Output, SimpleChange } from '@angular/core';
+import { Data, Options } from 'vis-network';
 
-import { Data, Options } from '.';
 import { VisNetworkService } from './vis-network.service';
 
 /**
