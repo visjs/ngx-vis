bac72dad80dec417f64aa63dd0a6e5291eb6498c
Micah Stubbs <micah.stubbs@gmail.com>
2022-04-12 12:48:14 -0700

    set mode in webpack config to fix warning

--- .config/webpack.dev.js
+++ .config/webpack.dev.js
@@ -17,9 +17,9 @@ module.exports = webpackMerge(commonConfig, {
     new MiniCssExtractPlugin({
       // Options similar to the same options in webpackOptions.output
       // both options are optional
-      filename: "[name].css",
-      chunkFilename: "[id].css"
-    })
+      filename: '[name].css',
+      chunkFilename: '[id].css',
+    }),
   ],
 
   devServer: {
@@ -27,5 +27,12 @@ module.exports = webpackMerge(commonConfig, {
     historyApiFallback: true,
     stats: 'minimal',
     watchContentBase: true,
-  }
+  },
+
+  /**
+   * Configure mode so that webpack can optimize the bundle accordingly
+   *
+   * See: https://webpack.js.org/configuration/mode/
+   */
+  mode: 'development',
 });
