f001bda63070f253342e99e409df0bf7a1fd468e
Micah Stubbs <micah.stubbs@gmail.com>
2022-04-12 12:50:53 -0700

    revert unwanted formatting changes to webpack.common.js

--- .config/webpack.common.js
+++ .config/webpack.common.js
@@ -6,13 +6,13 @@ var helpers = require('./helpers');
 
 module.exports = {
   entry: {
-    polyfills: helpers.root('demo', 'polyfills.ts'),
-    vendor: helpers.root('demo', 'vendor.ts'),
-    app: helpers.root('demo', 'index.ts'),
+    'polyfills': helpers.root('demo', 'polyfills.ts'),
+    'vendor': helpers.root('demo', 'vendor.ts'),
+    'app': helpers.root('demo', 'index.ts')
   },
 
   resolve: {
-    extensions: ['.ts', '.js'],
+    extensions: ['.ts', '.js']
   },
 
   module: {
@@ -22,18 +22,17 @@ module.exports = {
         loaders: [
           {
             loader: 'ts-loader',
-            options: { configFileName: helpers.root('tsconfig.json') },
-          },
-          'angular2-template-loader',
-        ],
+            options: { configFileName: helpers.root('tsconfig.json') }
+          } , 'angular2-template-loader'
+        ]
       },
       {
         test: /\.html$/,
-        loader: 'html-loader',
+        loader: 'html-loader'
       },
       {
         test: /\.(png|jpe?g|gif|svg|woff|woff2|ttf|eot|ico)$/,
-        loader: 'file-loader?name=assets/[name].[hash].[ext]',
+        loader: 'file-loader?name=assets/[name].[hash].[ext]'
       },
       {
         test: /\.css$/,
@@ -43,18 +42,18 @@ module.exports = {
             options: {
               // you can specify a publicPath here
               // by default it use publicPath in webpackOptions.output
-              publicPath: '../',
-            },
+              publicPath: '../'
+            }
           },
-          'css-loader',
-        ],
+          "css-loader"
+        ]
       },
       {
         test: /\.mjs$/,
         include: /node_modules/,
-        type: 'javascript/auto',
-      },
-    ],
+        type: "javascript/auto"
+      }
+    ]
   },
 
   plugins: [
@@ -67,9 +66,12 @@ module.exports = {
     ),
 
     new HtmlWebpackPlugin({
-      template: helpers.root('demo', 'index.html'),
+      template: helpers.root('demo', 'index.html')
     }),
 
-    new CopyWebpackPlugin([{ from: helpers.root('demo', 'assets'), to: 'assets' }]),
-  ],
+    new CopyWebpackPlugin([
+        { from: helpers.root('demo', 'assets'), to: 'assets' },
+      ]
+    )
+  ]
 };
