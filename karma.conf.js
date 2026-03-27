1f1f12b66eff7db65d3480c0378b919c4f456186
Martin Reinhardt <contact@martinreinhardt-online.de>
2026-03-27 07:43:41 +0100

    chore: Fix chrome sandbox issues on linux

--- karma.conf.js
+++ karma.conf.js
@@ -4,6 +4,12 @@ process.env.CHROME_BIN = require('puppeteer').executablePath();
 
 module.exports = function (config) {
   config.set({
+    customLaunchers: {
+      ChromeHeadlessCI: {
+        base: 'ChromeHeadless',
+        flags: ['--no-sandbox', '--disable-dev-shm-usage'],
+      },
+    },
     coverageReporter: {
       dir: 'coverage/',
       reporters: [
