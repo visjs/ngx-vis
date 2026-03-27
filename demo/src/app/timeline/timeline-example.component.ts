54fcf6abdcd1e070488b322d64f7429451de4633
Martin Reinhardt <contact@martinreinhardt-online.de>
2026-03-27 06:50:55 +0100

    chore: Use latest NodeJS

--- demo/src/app/timeline/timeline-example.component.ts
+++ demo/src/app/timeline/timeline-example.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnDestroy, OnInit } from "@angular/core";
-import { DataItem, VisTimelineService } from "ngx-vis";
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { DataItem, VisTimelineService } from 'ngx-vis';
 
 
 @Component({
